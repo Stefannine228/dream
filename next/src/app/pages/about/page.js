e7eb1c41706e22470d9938972694fdbab081fb3a
Roman Kravets <romann.kravets@gmail.com>
2025-03-11 19:26:07 +0200

    start

---
+++ next/src/app/pages/about/page.js
@@ -0,0 +1,8 @@
+'use client';
+import Map from "../../../components/map";
+
+export default function AboutPage() {
+  return (
+    <Map/>
+  );
+}
---
+++ next/src/components/map.js
@@ -0,0 +1,11 @@
+import styles from "../app/page.module.css";
+
+export default function Map() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+      <h1>Map</h1>
+      </main>
+    </div>
+  );
+}
